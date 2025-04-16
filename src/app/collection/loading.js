e02f9baabf88842393871a403aea51cae0899a4f
Issey Wu <wui12@mcmaster.ca>
2025-04-16 01:37:45 -0400

    Currently running into issues where TailwindCSS is not working and npm run dev server does not load. Exploring solutions

---
+++ src/app/collection/loading.js
@@ -0,0 +1,10 @@
+export default function CollectionLoading() {
+  return (
+    <div className="flex items-center justify-center min-h-[400px]">
+      <div className="text-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500 mx-auto"></div>
+        <p className="mt-4 text-lg text-gray-600">Loading devices...</p>
+      </div>
+    </div>
+  );
+}
